ba60168233ccd6b73dc6bcdcde5c354c32732202
Joe <jacaraccio@gmail.com>
2021-12-05 09:26:11 -0500

    base algorithm

---
+++ lib/Algorithims/HighGoal2020Algorithm.py
@@ -0,0 +1,7 @@
+# Algorithm for 2020 Game High Goal
+from .VisionAlgorithm import VisionAlgorithm
+
+class HighGoal2020Algorithm(VisionAlgorithm):
+
+    def __init__(self, name):
+        super().__init__(name)
